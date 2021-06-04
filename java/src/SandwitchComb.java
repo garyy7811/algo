a5015729995a41426bc2e3c5341233740806caa1
ubuntu <gary.y.7811@gmail.com>
2021-06-04 14:05:22 -0400

    back track solution

--- java/src/SandwitchComb.java
+++ java/src/SandwitchComb.java
@@ -1,14 +1,13 @@
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class SandwitchComb{
 
     public static void main( String[] args ){
-        class Solution{
+        class Solution1{
             private Map<Character, Set<Character>> typeToIngredients;
 
-            private Solution(){
+            private Solution1(){
                 typeToIngredients = new HashMap<>();
                 typeToIngredients.put( 'n', new HashSet<>( Arrays.asList( '1', '2' ) ) );
                 typeToIngredients.put( 'c', new HashSet<>( Arrays.asList( 'a', 'b', 'c' ) ) );
@@ -48,6 +47,55 @@ public class SandwitchComb{
             }
         }
 
-        System.out.println( new Solution().getAllCombine( 5 ) );
+        List<String> rslt1 = new Solution1().getAllCombine( 5 );
+
+        class Solution2{
+            private Map<Character, Set<Character>> typeToIngredients;
+
+            private Solution2(){
+                typeToIngredients = new HashMap<>();
+                typeToIngredients.put( 'n', new HashSet<>( Arrays.asList( '1', '2' ) ) );
+                typeToIngredients.put( 'c', new HashSet<>( Arrays.asList( 'a', 'b', 'c' ) ) );
+                typeToIngredients.put( 's', new HashSet<>( Arrays.asList( '~', '@' ) ) );
+                typeToIngredients.put( 'U', new HashSet<>( Arrays.asList( 'X', 'Y', 'Z' ) ) );
+            }
+
+            private List<String> getAllCombine( int layerNum ){
+                Map<Character, Character> ingreToType = new HashMap<>();
+                typeToIngredients.forEach( ( k, v ) -> {
+                    v.forEach( i -> {
+                        ingreToType.put( i, k );
+                    } );
+                } );
+                List<String> rslt = new ArrayList<>();
+                StringBuilder ss = new StringBuilder();
+                getAllCombine( rslt, ss, ingreToType.keySet(), ingreToType, layerNum );
+                return rslt;
+            }
+
+            private void getAllCombine( List<String> rslt, StringBuilder ss, Set<Character> keySet, Map<Character, Character> ingreToType, final int limit ){
+                if( ss.length() == limit ){
+                    rslt.add( ss.toString() );
+                    return;
+                }
+                getAvailableIngres( ss, keySet, ingreToType ).forEach( ni -> {
+                    ss.append( ni );
+                    getAllCombine( rslt, ss, keySet, ingreToType, limit );
+                    ss.deleteCharAt( ss.length() - 1 );
+                } );
+            }
+
+            private Set<Character> getAvailableIngres( StringBuilder ss, Set<Character> keySet, Map<Character, Character> ingreToType ){
+                return keySet.stream().filter( c -> ss.length() == 0 || ingreToType.get( c ) !=  ingreToType.get( ss.charAt( ss.length() - 1 ) ) ).collect( Collectors.toSet() );
+            }
+        }
+
+        List<String> rslt2 = new Solution2().getAllCombine( 5 );
+
+        System.out.println( rslt1 );
+        System.out.println( "-------------------------------------" );
+        System.out.println( rslt2 );
+
+
     }
 }
